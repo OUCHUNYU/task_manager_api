7609e50b9cbfcfe1c65ba75fde26bb50f1462329
OUCHUNYU <ouchunyu@yahoo.com>
2016-07-31 19:32:37 -0700

    complete user and project routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
   def create
-
+    hash_id = SecureRandom.base64
+    user = User.new(hash_id: hash_id)
+    if user.valid?
+      user.save
+      render json: { hash_id: hash_id, user_object: user.projects }
+    else
+      render json: { message: "something went wrong" }
+    end
   end
 
   def show
-
+    user = User.find_by(hash_id: params[:id])
+    if params[:id] && user
+      render json: user.projects
+    else
+      render json: { message: "User not found" }
+    end
   end
+
+  # private
+  # def hash_id_params
+  #   params.permit(:id)
+  # end
 end
