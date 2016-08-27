1178f0ad5cc2e78785e8f7fb8f1a63e6b87dd15b
Kevin Niu <kev.niu@gmail.com>
2016-08-27 16:14:08 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/users/position_order', to: 'users#update_position_order'
+  put '/projects/task_order', to: 'projects#update_task_order'
 
   resources :users, only: [:create, :show]
   resources :projects, only: [:create, :update, :destroy, :show]
